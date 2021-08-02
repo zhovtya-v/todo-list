17fff6b34e3ebada5042b10bff93a433e691a642
Vika <vorona.v.w@gmail.com>
2021-08-02 09:22:01 +0300

    Add components folder

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import TodoListItem from './TodoListItem'
 
+import './TodoList.css'
+
 const TodoList = ({todos}) => {
-    const elements = todos.map((item) => <li key={item.id}><TodoListItem { ...item}/></li>);
+    const elements = todos.map((item) => {
+
+        const { id, ...itemProps } = item;
+
+        return (
+            <li key={id} className='list-group-item'>
+                <TodoListItem { ...itemProps}/>
+            </li>);
+    });
 
     return (
-        <ul>{elements}</ul>
+        <ul className='list-group todo-list'>{elements}</ul>
     )
 }
 
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
+import './TodoListItem.css'
+
 const TodoListItem = ( {label, important = false} ) => {
 
     const style = {
         color: important ? 'tomato' : 'black'
     }
 
-    return <span style={style}>{ label }</span>
+    return ( 
+        <span className='todo-list-item' style={style}>{ label }</span>
+    )
 }
 
 export default TodoListItem;
